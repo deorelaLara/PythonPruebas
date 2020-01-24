36790793d61c6a6cea39437a87c5dc8b04b87b7e
deorela Lara <l.lara@uadec.edu.mx>
2020-01-24 16:16:37 -0600

    Primeros archivos agregados

---
+++ ContadorPalabras.py
@@ -0,0 +1,34 @@
+"""Escribir una aplicacion llamada "Contador de Palabras",
+   que lea lineas de un archivo tipo texto, hasta que encuentre el fin del archivo
+
+   La aplicacion debe indicar cuantas palabras tiene cada linea, una palabra es un conjunto
+   de caracteres separados por uno oo mas espacios. Y tambien debe indicar el numero de lineas leidas.
+
+   Alumno(a) : ALVARADO LARA LUZ DEORELA SABAS
+   Matricula: 10021265
+
+   """
+
+def contarPalabras(archivo = 'test.txt'):
+    local = open(archivo, 'r')
+    lineas = local.readlines()
+    for i in range(0,len(lineas)):
+        print('línea {}: {} palabras'.format(i+1, len(lineas[i].split())))
+
+def contarLineas(archivo = 'test.txt'):
+    local = open(archivo, 'r')
+    print('total {} líneas.'.format(len(local.readlines())))
+    local.close()
+
+def main():
+    # Prueba #1
+    # contarLineas()
+    # Prueba #2
+    # print('Este es     un   ejemplo'.split())
+    # Prueba #3
+    # contarPalabras()
+    contarPalabras()
+    contarLineas()
+
+if __name__ == '__main__':
+    main()
